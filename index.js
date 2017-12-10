3e1e0fecca11ae0a5272b8b8ba727ac91baf2f84
Ali Ilman <aliilman532@gmail.com>
2017-12-10 19:01:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function getCount(str) {
   return stringMatch ? stringMatch.length : 0;
 }
 getCount("Morning Glory");
+
+//#2 Square Every Digit
+function squareDigits(number) {
+  numArr = number.toString().split("");
+  return parseInt(numArr.map((num) => {
+    stringToNum = parseInt(num);
+    return stringToNum ** 2;
+  }).join(""));
+}
+squareDigits(25);
+
+//#3 Jaden Casing Strings
+String.prototype.toJadenCase = function () {
+  return this.split(" ").map(word => {
+    return word.replace(word[0], word[0].toUpperCase());
+  }).join(" ");
+};
+
+"Hello, i am ali".toJadenCase();
