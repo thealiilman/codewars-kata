01e721129a8ea5b9e0f70f887a9684bf08bcacf9
Ali Ilman <aliilman532@gmail.com>
2017-12-11 15:32:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,45 @@ String.prototype.toJadenCase = function () {
 };
 
 "Hello, i am ali".toJadenCase();
+
+//#4 Shortest Word
+function findShort(str){
+  const stringArr = str.split(" ");
+  let wordLength = 0;
+  
+  for(let x = 0; x < stringArr.length; x++) {
+    if(wordLength === 0) wordLength = stringArr[0].length;
+    if(stringArr[x].length < wordLength) wordLength = stringArr[x].length;
+  }
+  return wordLength;
+}
+findShort("To sing my soul to sleep and take me back to bed");
+
+//#5 The Highest Profit Wins!
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)];
+}
+minMax([1, 5, 10, 15]);
+
+//#6 String Repeat
+function repeatStr (n, s) {
+  let repeatedStr = "";
+  for(let x = 0; x < n; x++) {
+    repeatedStr += s;
+  }
+  return repeatedStr;
+}
+repeatStr(4, "Well...")
+
+//#7 Double Char
+function doubleChar(str) {
+  return str.split("").map(letter => letter += letter).join("");
+}
+doubleChar("Oasis");
+
+//#8 Sum of Two Lowest Positive Integers
+function sumTwoSmallestNumbers(numbers) {  
+  const copyArr = [...numbers];
+  return copyArr.filter(num => num >= 0).sort((a, b) => a-b).slice(0, 2).reduce((a, b) => a+b);
+};
+sumTwoSmallestNumbers([-5, -1, 4, 1, 5, 10, 15]);
